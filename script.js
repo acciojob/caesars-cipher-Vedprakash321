e11d7c0bb77a4cf525c773a5e55b8a5d0370b0e1
Vedprakash321 <131158289+Vedprakash321@users.noreply.github.com>
2023-09-12 15:01:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,29 +30,26 @@ const lookup = {
   "?": "?",
   ",": ",",
 };
-// function rot13(encodedString) {
-//   return encodedString.replace(/[A-Z]/g, (char) => {
-//     const charCode = char.charCodeAt(0);
-//     const base = charCode < 78 ? 65 : 97; // Determine if the character is uppercase or lowercase
-//     return String.fromCharCode(base + ((charCode - base + 13) % 26));
-//   });
-// }
+
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
-	return encodedString.replace(/[A-Z]/g, (char) => {
-    const charCode = char.charCodeAt(0);
-    const base = charCode < 78 ? 65 : 97; // Determine if the character is uppercase or lowercase
-    return String.fromCharCode(base + ((charCode - base + 13) % 26));
-  });
-	
+	let solved="";
+	let n=encodedStr.length;
+	for(let i=0;i<n;i++){
+		var asciiNum==str[i].charCodeAt();
+		if(asciiNum>=65 && asciiNum<=90){
+			solved+=String.fromCharCode(asciiNum + 13);
+		}
+		
+	}
 
-  return; //return decodedArr
+  return solved; //return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(rot13("SERR YBIR? NPPVBWBO"));
+// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 // Do not change this line
 window.rot13 = rot13;
